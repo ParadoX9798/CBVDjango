60491f83e58d5fb87650a897e193def8bfd8763a
ParadoX <youneskazemi9798@gmail.com>
2021-10-04 12:23:11 +0330

    DetailView

--- first/urls.py
+++ first/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 app_name = "first"
 urlpatterns = [
-    path('', views.Home.as_view(), name="hello")
+    path('', views.Home.as_view(), name="hello"),
+    path('detail_todo/<slug:slug>/', views.DetailTodo.as_view(), name="detail_todo")
 ]
--- first/views.py
+++ first/views.py
@@ -1,12 +1,34 @@
 from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Todo
 
 
-class Home(TemplateView):
-    template_name = "first/hello.html"
+# class Home(TemplateView):
+#     template_name = "first/hello.html"
+#
+#     def get_context_data(self, **kwargs):
+#         contex = super().get_context_data(**kwargs)
+#         contex['todos'] = Todo.objects.all()
+#         return contex
 
-    def get_context_data(self, **kwargs):
-        contex = super().get_context_data(**kwargs)
-        contex['todos'] = Todo.objects.all()
+
+class Home(ListView):
+    template_name = "first/hello.html"
+    model = Todo
+    context_object_name = "todos"
+    ordering = ("-created",)
+
+
+class DetailTodo(DetailView):
+    template_name = "first/detail_todo.html"
+    slug_field = "slug"
+    slug_url_kwarg = "slug"
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            return Todo.objects.filter(slug=self.kwargs['slug'])
+        else:
+            return Todo.objects.none()
